99fd74a28d15a6086621f0927d97ce797c95c1a3
MalikDzh <944malik56@gamil.com>
2022-12-24 22:35:20 +0600

    working_with_add

--- src/components/Admin/Add/Add.jsx
+++ src/components/Admin/Add/Add.jsx
@@ -1,10 +1,13 @@
+import { Box, Button, TextField, Typography } from "@mui/material"
 import React, { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom";
 import { productContext } from "../../../Context/ProductContextProvider"
 
 const Add = () => {
-	const { addProd } = useContext(productContext)
+	const { addProd } = useContext(productContext);
+	const navigate = useNavigate();
 
-	const [prod, setProd] = useState({
+	const [products, setProd] = useState({
 		title: "",
 		img: "",
 		price: 0,
@@ -12,15 +15,65 @@ const Add = () => {
 
 	const collectInp = (e) => {
 		if (e.target.name === "price") {
-			let obj = { ...prod, [e.target.name]: Number(e.target.value) }
+			let obj = { ...products, [e.target.name]: Number(e.target.value) }
 			setProd(obj)
 		} else {
-			let obj = { ...prod, [e.target.name]: e.target.value }
+			let obj = { ...products, [e.target.name]: e.target.value }
 			setProd(obj)
 		}
 	}
 
-	return <div></div>
+	return (
+		<div>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography>Add Product Panel</Typography>
+        <TextField
+          onChange={collectInp}
+          name="title"
+          id="outlined-basic"
+          label="Title"
+          variant="outlined"
+        />
+        <TextField
+          onChange={collectInp}
+          name="img"
+          id="outlined-basic"
+          label="Image"
+          variant="outlined"
+        />
+        <TextField
+          onChange={collectInp}
+          name="price"
+          id="outlined-basic"
+          label="Price"
+          variant="outlined"
+        />
+        <TextField
+          onChange={collectInp}
+          name="type"
+          id="outlined-basic"
+          label="Type"
+          variant="outlined"
+        />
+        <Button
+          onClick={() => {
+            addProd(products);
+            navigate("/products");
+          }}
+          variant="outlined"
+        >
+          Add prod
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default Add
